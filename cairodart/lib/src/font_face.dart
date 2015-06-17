6bf5d86573ed0b878e5ae1f84b7f32986a45dcf1
Alexander Dembinsky <alexander.dembinsky@gmail.com>
2015-06-17 20:22:46 +0300

    Implemented user_font_face_create() function. Added several unit tests for ScaledFont.

--- cairodart/lib/src/font_face.dart
+++ cairodart/lib/src/font_face.dart
@@ -3,14 +3,22 @@ part of cairodart.base;
 abstract class FontFace {
 
   factory FontFace._internal() => new _FontFace();
+
+  factory FontFace.userFont() => new _FontFace.userFont();
   
   FontType get type;
   
 }
 
-class _FontFace implements FontFace {
+class _FontFace extends NativeFieldWrapperClass2 implements FontFace {
   
   FontType get type native 'font_face_get_type';
+
+  _FontFace.userFont() {
+    _createUserFont();
+  }
+
+  _createUserFont() native 'user_font_face_create';
   
 }
 
--- native/font_face.h
+++ native/font_face.h
@@ -5,5 +5,6 @@
 
 void font_face_destroy(void* handle);
 void font_face_get_type(Dart_NativeArguments args);
+void user_font_face_create(Dart_NativeArguments args);
 
 #endif // FONT_FACE_H
